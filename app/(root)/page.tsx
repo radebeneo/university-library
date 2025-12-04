3e40f6e564fac60ad43f66e5bee7c629f06c3697
radebeneo <radebeneo17@gmail.com>
2025-12-04 12:02:51 +0200

    can render book-overview and book-list in the home page.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
 import {Button} from "@/components/ui/button";
+import BookOverview from "@/components/BookOverview";
+import BookList from "@/components/BookList";
+
+
 
 const Home = () => {
   return (
     <>
+        <BookOverview/>
+        <BookList />
 
     </>
   );
---
+++ components/BookList.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const BookList = () => {
+    return (
+        <section className="font-bold text-4xl text-light-100">
+            Popular Books
+        </section>
+    )
+}
+export default BookList
