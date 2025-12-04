3a86ba4ec45fc6ae1558532460446f8663c6c3ea
radebeneo <radebeneo17@gmail.com>
2025-12-04 15:29:36 +0200

    rendered book details, using bookCover and bookOverview component.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,14 +2,19 @@ import Image from "next/image";
 import {Button} from "@/components/ui/button";
 import BookOverview from "@/components/BookOverview";
 import BookList from "@/components/BookList";
+import {sampleBooks} from "@/constants";
 
 
 
 const Home = () => {
   return (
     <>
-        <BookOverview/>
-        <BookList />
+        <BookOverview {...sampleBooks[0]}/>
+        <BookList
+        title = "Latest Books"
+        books={sampleBooks}
+        containerClassName="mt-28"
+        />
 
     </>
   );
