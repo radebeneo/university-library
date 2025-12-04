ebaef476f77415d548112c81f3524108207abfc7
radebeneo <radebeneo17@gmail.com>
2025-12-04 09:54:33 +0200

    Implemented Tailwind v4 theming in global.css, refactored from Tailwind v3 style theming.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "tailwindcss";
 import animate from "tailwindcss-animate";
 
 export default {
+    important: true,
   darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
