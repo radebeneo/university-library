ab5977ebdf39b94ce4f93ca6373da3c2432b399e
radebeneo <radebeneo17@gmail.com>
2025-12-10 21:50:53 +0200

    getting errors with next-image and imagekit-image, using the same identifier.

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -1,10 +1,51 @@
 'use client'
 
-import { Image, ImageKitProvider, upload } from '@imagekit/next'
+import { ImageKitProvider,Image,  upload } from '@imagekit/next'
+import config from "@/lib/config";
+import {useRef, useState} from "react";
+import Image from "next/image";
 
+const { env: {imagekit: {publicKey, urlEndpoint}}} = config;
+const authenticator = async ()=> {
+    try{
+        const response = await fetch(`${config.env.apiEndpoint}/api/auth/imagekit`)
+
+        if (!response.ok) {
+            const errorText = await response.text()
+            throw new Error(`Request failed with status ${response.status}: ${errorText}`)
+        }
+
+        const data = await response.json()
+
+        const {signature, expire, token} = data
+
+        return {signature, expire, token}
+
+    } catch (error: any) {
+        throw new Error(`Authentication request failed: ${error.message}`)
+    }
+}
 const ImageUpload = () => {
-    return (
-        <div>ImageUpload</div>
+    const ikUploadRef = useRef(null)
+
+    const [file, setFile] = useState<{filePath: string} | null>(null)
+
+    const onError = () => {}
+    const onSuccess = () => {}
+
+    return(
+        <ImageKitProvider publicKey={publicKey} urlEndpoint={urlEndpoint} authenticator={authenticator}>
+            <upload className="hidden" ref={ikUploadRef} onError={onError} onSuccess={onSuccess} fileName="test-upload.png"/>
+            <button className="upload-btn">
+                <Image src="/icons/upload.svg" alt="upload-icon" width={20} height={20} className="object-contain"/>
+                <p className="text-base text-light-100">Upload Student Card</p>
+                {file && <p className="upload-filename">{file.filePath}</p>}
+            </button>
+            {file && (
+                <Image alt={file.filePath} path={file.filePath} width={500} height={500}/>
+            )}
+
+        </ImageKitProvider>
     )
 }
 export default ImageUpload
