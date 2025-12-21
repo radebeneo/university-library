62bf5f4f37b89ea133b96fdcd21feeedb8fd7af0
radebeneo <radebeneo17@gmail.com>
2025-12-21 16:03:06 +0200

    added callback functions for jwt and session.

--- auth.ts
+++ auth.ts
@@ -38,5 +38,26 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
 
         })
     ],
+    pages: {
+        signIn: "/sign-in",
+    },
+    callbacks: {
+        async jwt ({token, user}){
+            if(user){
+                token.id = user.id
+                token.name = user.name
+            }
+
+            return token
+        },
+        async session ({session, token}){
+            if(session.user){
+                session.user.id = token.id as string
+                session.user.name = token.name as string
+            }
+
+            return session
+        }
+    },
 
 })
