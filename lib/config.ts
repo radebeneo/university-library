907d76a0fc90ab6d3bc85de2cda171414df0bfaf
radebeneo <radebeneo17@gmail.com>
2026-03-12 22:43:44 +0200

    setup upstash redis, and rate limit logic.

--- lib/config.ts
+++ lib/config.ts
@@ -5,7 +5,11 @@ const config = {
             publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
             urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!
         },
-        databaseUrl: process.env.DATABASE_URL!
+        databaseUrl: process.env.DATABASE_URL!,
+        upstash: {
+            redisUrl: process.env.UPSTASH_REDIS_REST_URL,
+            redisToken: process.env.UPSTASH_REDIS_REST_TOKEN,
+        }
     }
 }
 
