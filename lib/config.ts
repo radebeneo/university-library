6162caf52effe08c9d958c7a0786d59f0a143ff5
radebeneo <radebeneo17@gmail.com>
2026-03-15 09:15:54 +0200

    setup workflow client.

--- lib/config.ts
+++ lib/config.ts
@@ -7,10 +7,10 @@ const config = {
         },
         databaseUrl: process.env.DATABASE_URL!,
         upstash: {
-            redisUrl: process.env.UPSTASH_REDIS_REST_URL,
-            redisToken: process.env.UPSTASH_REDIS_REST_TOKEN,
-            qstashUrl: process.env.UPSTASH_QSTASH_URL,
-            qstashToken: process.env.UPSTASH_QSTASH_TOKEN,
+            redisUrl: process.env.UPSTASH_REDIS_REST_URL!,
+            redisToken: process.env.UPSTASH_REDIS_REST_TOKEN!,
+            qstashUrl: process.env.UPSTASH_QSTASH_URL!,
+            qstashToken: process.env.UPSTASH_QSTASH_TOKEN!,
         }
     }
 }
